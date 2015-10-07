72e007078af6d2e7bfc5b43fc1d6074cd63e569d
ke <sunke.polyu@gmail.com>
2015-10-07 14:52:00 -0400

    add tempate header and src file for the driver

---
+++ include/camera_imu_sync/CamImuSynchronizer.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright [2015] [Ke Sun  sunke.polyu@gmail.com]
+ *                  [Chao Qu quchao@seas.upenn.edu]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CAM_IMU_SYNC
+#define CAM_IMU_SYNC
+
+#include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <ros/ros.h>
+#include <imu_vn_100/imu_ros_base.h>
+// TODO: include the header files for the camera driver
+
+
+namespace cam_imu_sync {
+
+/**
+ * @brief CamImuSynchronizer
+ *        The class is used for synchronize the time stamp for
+ *        imu and cameras. Currently, it only supports imu VN100
+ *        from VECTORNAV and cameras from BLUEFOX. The cameras
+ *        are supposed to be externally triggered by the imu.
+ * @author Ke Sun
+ */
+class CamImuSynchronizer {
+  public:
+    CamImuSynchronizer(const ros::NodeHandle& n);
+    ~CamImuSynchronizer();
+
+    /**
+     * @brief initialize Initialize IMU and CAM objects
+     * @return True If the driver is initialized successfully.
+     */
+    bool initialize();
+
+    /**
+     * @brief start Starts the driver(IMU and CAM(s))
+     */
+    void start();
+
+  private:
+    // IMU object
+    imu_vn_100::ImuRosBase imu;
+
+    // TODO: Camera object(s)
+
+    // Ros node
+    ros::NodeHandle nh;
+
+    // A seperate thread waiting for images
+    boost::shared_ptr<boost::thread> img_poll_thread_ptr;
+
+    // Poll image(s) from camera(s)
+    //    This function will be run on a seperate thread
+    //    avoid blocking the main function.
+    void pollImage();
+
+    // Disable copy and assign contructor
+    CamImuSynchronizer(const CamImuSynchronizer&);
+    CamImuSynchronizer& operator=(const CamImuSynchronizer&);
+};
+
+}
+
+#endif
---
+++ src/CamImuSynchronizer.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright [2015] [Ke Sun  sunke.polyu@gmail.com]
+ *                  [Chao Qu quchao@seas.upenn.edu]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+#include <cam_imu_sync/CamImuSynchronizer.h>
+
+namespace cam_imu_sync {
+
+CamImuSynchronizer::CamImuSynchronizer(
+    const ros::Nodehandle& n):
+  nh(n),
+  imu(n)
+  // TODO: pass the ros node handle to camera objects
+  {
+  return;
+}
+
+bool CamImuSynchronizer::initialize() {
+  // Initialize IMU
+  if(!imu.initialize()) {
+    ROS_ERROR("Fail to initialize IMU.");
+    return false;
+  }
+  // TODO: intialize camera(s)
+
+  return true;
+}
+
+void CamImuSynchronizer::start() {
+  // Start the IMU streaming
+  imu.enableIMUStream();
+  // Start polling images from the camera(s)
+  img_poll_thread_ptr = new boost::thread(
+      &CamImuSynchronizer::pollImage, this);
+  return;
+}
+
+void CamImuSynchronizer::pollImage() {
+  while (true) {
+    // bool new_flag = cam.getImage()
+    // if (new_flag) {
+    //  ros::Time new_time_stamp = imu.getSyncTime();
+    //  cam.publishImage(new_time_stamp);
+    // }
+  }
+  return;
+}
+
+
+}
